2cdc960491fe86869fd8c6cc2362b98beae628fe
Zaki Kurdya <46399191+ZakiKurdya@users.noreply.github.com>
2024-11-30 22:27:04 +0200

    Refactor llm_crud and add sort key

--- backend/llm_crud/llm_crud.py
+++ backend/llm_crud/llm_crud.py
@@ -1,59 +1,73 @@
 import json
 import boto3
 
-client = boto3.client('dynamodb')
+client = boto3.client("dynamodb")
 dynamodb = boto3.resource("dynamodb")
-table = dynamodb.Table('LLMs') 
+table = dynamodb.Table("LLMs")
+
 
 def lambda_handler(event, context):
     print(event)
     body = {}
     statusCode = 200
-    headers = {
-        "Content-Type": "application/json"
-    }
+    headers = {"Content-Type": "application/json"}
 
     try:
-        if event['httpMethod'] == "DELETE" and event['pathParameters'] is not None :
+        if event["httpMethod"] == "DELETE" and event["pathParameters"] is not None:
+            provider = event["pathParameters"]["provider"]
+            name = event["pathParameters"]["name"]
+
             table.delete_item(
-                Key={'name': str(event['pathParameters']['name'])})
-            body = 'Deleted item ' + str(event['pathParameters']['name'])
-        elif event['httpMethod'] == "GET" and event['pathParameters'] is not None :
+                Key={"name": name, "provider": provider}  # Partition key  # Sort key
+            )
+            body = f"Deleted item: {name}-{provider}"
+        elif event["httpMethod"] == "GET" and event["pathParameters"] is not None:
             body = table.get_item(
-                Key={'name': str(event['pathParameters']['name'])})
-            body = body["Item"]
-            responseBody = [
-                {'name': body['name'], 'quality': body['quality']}]
-            body = responseBody
-        elif event['httpMethod'] == "GET"  and event['pathParameters'] is None:
+                Key={
+                    "name": str(event["pathParameters"]["name"]),
+                    "provider": str(event["pathParameters"]["provider"]),
+                }
+            )
+            body = [
+                {
+                    "name": body["Item"]["name"],
+                    "provider": body["Item"]["provider"],
+                    "quality": body["Item"]["quality"],
+                }
+            ]
+        elif event["httpMethod"] == "GET" and event["pathParameters"] is None:
             body = table.scan()
             body = body["Items"]
-            print("ITEMS----")
-            print(body)
             responseBody = []
             for items in body:
                 responseItems = [
-                    {'name': items['name'], 'quality': items['quality']}]
+                    {
+                        "name": items["name"],
+                        "provider": items["provider"],
+                        "quality": items["quality"],
+                    }
+                ]
                 responseBody.append(responseItems)
             body = responseBody
-        elif event['httpMethod'] == "PUT":
-            requestJSON = json.loads(event['body'])
+        elif event["httpMethod"] == "PUT":
+            requestJSON = json.loads(event["body"])
             table.put_item(
                 Item={
-                    # 'id': requestJSON['id'],
-                    'name': requestJSON['name'],
-                    'quality': requestJSON['quality']
-                })
-            body = 'Put item ' + requestJSON['name']
+                    "name": requestJSON["name"],
+                    "provider": requestJSON["provider"],
+                    "quality": requestJSON["quality"],
+                }
+            )
+            body = f"Put item: {prrequestJSON["name"]}-{prrequestJSON["provider"]}"
     except KeyError:
         statusCode = 400
-        body = 'Unsupported route: ' + event['path']
+        body = "Unsupported route: " + event["path"]
+
     body = json.dumps(body)
     res = {
         "statusCode": statusCode,
-        "headers": {
-            "Content-Type": "application/json"
-        },
-        "body": body
+        "headers": {"Content-Type": "application/json"},
+        "body": body,
     }
+
     return res
--- infrastructure/infrastructure/infrastructure_stack.py
+++ infrastructure/infrastructure/infrastructure_stack.py
@@ -6,79 +6,105 @@ from aws_cdk import (
     aws_lambda as lambda_,
     aws_apigateway as apigateway,
     aws_dynamodb as dynamodb,
-    aws_iam as iam
+    aws_iam as iam,
 )
 
 
 class InfrastructureStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, env_name: str, lambda_memory_size: int, lambda_timeout: int, OPENROUTER_API_KEY: str, LANGCHAIN_API_KEY: str, **kwargs) -> None:
+    def __init__(
+        self,
+        scope: Construct,
+        construct_id: str,
+        env_name: str,
+        lambda_memory_size: int,
+        lambda_timeout: int,
+        OPENROUTER_API_KEY: str,
+        LANGCHAIN_API_KEY: str,
+        **kwargs,
+    ) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         # Create the IAM User Groups and Policies
         # Backend Developers Group
         backend_policy = iam.ManagedPolicy(
-            self, "BackendPolicy",
+            self,
+            "BackendPolicy",
             statements=[
                 iam.PolicyStatement(
-                    actions=["lambda:*", "dynamodb:*", "apigateway:*",
-                             "cognito-idp:*", "cognito-identity:*", "cognito-sync:*",
-                             "rds:*", "s3:*", "ec2:*", "amplify:*"],
-                    resources=["*"]
+                    actions=[
+                        "lambda:*",
+                        "dynamodb:*",
+                        "apigateway:*",
+                        "cognito-idp:*",
+                        "cognito-identity:*",
+                        "cognito-sync:*",
+                        "rds:*",
+                        "s3:*",
+                        "ec2:*",
+                        "amplify:*",
+                    ],
+                    resources=["*"],
                 )
-            ]
+            ],
         )
         backend_group = iam.Group(self, "BackendGroup")
         backend_group.add_managed_policy(backend_policy)
 
         # Frontend Developers Group
         frontend_policy = iam.ManagedPolicy(
-            self, "FrontendPolicy",
+            self,
+            "FrontendPolicy",
             statements=[
                 iam.PolicyStatement(
-                    actions=["dynamodb:List*", "dynamodb:Read*",
-                             "s3:List*", "s3:Read*",
-                             "rds:List*", "rds:Read*",
-                             "amplify:*"],
-                    resources=["*"]
+                    actions=[
+                        "dynamodb:List*",
+                        "dynamodb:Read*",
+                        "s3:List*",
+                        "s3:Read*",
+                        "rds:List*",
+                        "rds:Read*",
+                        "amplify:*",
+                    ],
+                    resources=["*"],
                 )
-            ]
+            ],
         )
         frontend_group = iam.Group(self, "FrontendGroup")
         frontend_group.add_managed_policy(frontend_policy)
 
         # Admin Group
         admin_policy = iam.ManagedPolicy(
-            self, "AdminPolicy",
-            statements=[
-                iam.PolicyStatement(
-                    actions=["*"],
-                    resources=["*"]
-                )
-            ]
+            self,
+            "AdminPolicy",
+            statements=[iam.PolicyStatement(actions=["*"], resources=["*"])],
         )
         admin_group = iam.Group(self, "AdminGroup")
         admin_group.add_managed_policy(admin_policy)
 
         # Create Backend Users
-        iam.User(self, "BackendUser-Mohammed",
-                 user_name="backend-user-mohammed",
-                 groups=[backend_group]
-                )
-            
+        iam.User(
+            self,
+            "BackendUser-Mohammed",
+            user_name="backend-user-mohammed",
+            groups=[backend_group],
+        )
+
         # Create Frontend Users
-        iam.User(self, "FrontendUser-Shady",
-                 user_name="frontend-user-shady",
-                 groups=[frontend_group]
-                )
-        
+        iam.User(
+            self,
+            "FrontendUser-Shady",
+            user_name="frontend-user-shady",
+            groups=[frontend_group],
+        )
+
         # Create Admin Users
-        iam.User(self, "AdminUser-Zaki",
-                 user_name="admin-user-zaki",
-                 groups=[admin_group])
-        iam.User(self, "AdminUser-Salem",
-                 user_name="admin-user-salem",
-                 groups=[admin_group])         
+        iam.User(
+            self, "AdminUser-Zaki", user_name="admin-user-zaki", groups=[admin_group]
+        )
+        iam.User(
+            self, "AdminUser-Salem", user_name="admin-user-salem", groups=[admin_group]
+        )
         lambda_role = iam.Role(
             self,
             id=f"{env_name}-LLMCallLambdaRole",
@@ -86,35 +112,37 @@ class InfrastructureStack(Stack):
         )
 
         # Add policies to the role
-      
+
         lambda_role.add_managed_policy(
-            iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AWSLambdaBasicExecutionRole")
+            iam.ManagedPolicy.from_aws_managed_policy_name(
+                "service-role/AWSLambdaBasicExecutionRole"
+            )
         )
         lambda_role.add_managed_policy(
             iam.ManagedPolicy.from_aws_managed_policy_name("AmazonDynamoDBFullAccess")
         )
         # LLM Calling Lambda Function ---------------------------------
-        '''
+        """
         # Add lambda layer by ARN
         llm_call_lambda_layer = lambda_.LayerVersion.from_layer_version_arn(
             self, 
             "LLMsBasicDependencies",
             "arn:aws:lambda:eu-north-1:***REMOVED-AWS-ACCOUNT-ID***:layer:LLMsBasicsDependencies:1"
         )
-        '''
+        """
 
         llm_call_lambda_layer = lambda_.LayerVersion(
-            self, "LLMsBasicDependencies",
+            self,
+            "LLMsBasicDependencies",
             code=lambda_.Code.from_asset("./layers/llms_basic_dependencies/"),
             compatible_runtimes=[lambda_.Runtime.PYTHON_3_10],
             description="Lambda layer for LangChain, LiteLLM and other required dependencies",
         )
-                
+
         llm_call_lambda_code_path = os.path.join(
-            os.path.dirname(__file__), 
-            "../../backend/llm_call"
+            os.path.dirname(__file__), "../../backend/llm_call"
         )
-    
+
         llm_call_function = lambda_.Function(
             self,
             id=f"{env_name}-LLMCallFunction",
@@ -125,30 +153,28 @@ class InfrastructureStack(Stack):
             timeout=Duration.seconds(lambda_timeout),
             memory_size=lambda_memory_size,
             environment={
-                "ENV_NAME":env_name,
-                "LANGCHAIN_TRACING_V2":"true",
-                "LANGCHAIN_ENDPOINT":"https://api.smith.langchain.com",
+                "ENV_NAME": env_name,
+                "LANGCHAIN_TRACING_V2": "true",
+                "LANGCHAIN_ENDPOINT": "https://api.smith.langchain.com",
                 "OPENROUTER_API_KEY": OPENROUTER_API_KEY,
                 "LANGCHAIN_PROJECT": "Taklif.AI",
                 "LANGCHAIN_API_KEY": LANGCHAIN_API_KEY,
-            }
+            },
         )
-
-   
 
         # LLM crud Lambda Function ---------------------------------
         llm_crud_lambda_layer = lambda_.LayerVersion(
-            self, "LLMCrudDependencies",
+            self,
+            "LLMCrudDependencies",
             code=lambda_.Code.from_asset("./layers/llm_crud/"),
             compatible_runtimes=[lambda_.Runtime.PYTHON_3_10],
             description="Lambda layer for boto3 and others.",
         )
-                
+
         llm_crud_lambda_code_path = os.path.join(
-            os.path.dirname(__file__), 
-            "../../backend/llm_crud"
+            os.path.dirname(__file__), "../../backend/llm_crud"
         )
-    
+
         llm_crud_function = lambda_.Function(
             self,
             id=f"{env_name}-LLMCrudFunction",
@@ -159,10 +185,9 @@ class InfrastructureStack(Stack):
             timeout=Duration.seconds(lambda_timeout),
             memory_size=lambda_memory_size,
             environment={
-                "ENV_NAME":env_name,
+                "ENV_NAME": env_name,
             },
-            role=lambda_role, 
-
+            role=lambda_role,
         )
 
         llm_api = apigateway.RestApi(
@@ -171,11 +196,11 @@ class InfrastructureStack(Stack):
             rest_api_name=f"{env_name}-LLMCallAPI",
             description="API Gateway for LLM Call",
             endpoint_types=[apigateway.EndpointType.EDGE],
-            deploy_options=apigateway.StageOptions( # we can make the throttling limits dynamic by the user subscription
-            stage_name=env_name,
-#                throttling_rate_limit=1, # maximum number of requests per second (RPS) allowed for the stage
-#                throttling_burst_limit=5, # maximum number of requests that can be served in a short burst before the rate limit is applied
-            )
+            deploy_options=apigateway.StageOptions(  # we can make the throttling limits dynamic by the user subscription
+                stage_name=env_name,
+                #                throttling_rate_limit=1, # maximum number of requests per second (RPS) allowed for the stage
+                #                throttling_burst_limit=5, # maximum number of requests that can be served in a short burst before the rate limit is applied
+            ),
         )
 
         # llm_call_resource = llm_api.root.add_resource("llm_call")
@@ -184,24 +209,42 @@ class InfrastructureStack(Stack):
         #     allow_origins=apigateway.Cors.ALL_ORIGINS,  # Allow all origins, or specify a list of allowed origins (it can be replaced with our frontend domain)
         # )
 
-        
         llm_call_resource = llm_api.root.add_resource("llm_call")
-        llm_call_resource.add_method("POST", apigateway.LambdaIntegration(llm_call_function))
+        llm_call_resource.add_method(
+            "POST", apigateway.LambdaIntegration(llm_call_function)
+        )
         llm_call_resource.add_cors_preflight(
             allow_origins=apigateway.Cors.ALL_ORIGINS,  # Allow all origins, or specify a list of allowed origins (it can be replaced with our frontend domain)
         )
         # Create a resource for routing Lambda 2
         items_resource = llm_api.root.add_resource("items")
-        items_resource.add_method("GET", apigateway.LambdaIntegration(llm_crud_function))
-        items_resource.add_method("PUT", apigateway.LambdaIntegration(llm_crud_function))
+        items_resource.add_method(
+            "GET", apigateway.LambdaIntegration(llm_crud_function)
+        )
+        items_resource.add_method(
+            "PUT", apigateway.LambdaIntegration(llm_crud_function)
+        )
         item_name_resource = items_resource.add_resource("{name}")
-        item_name_resource.add_method("DELETE", apigateway.LambdaIntegration(llm_crud_function)) # NAME
-        item_name_resource.add_method("GET", apigateway.LambdaIntegration(llm_crud_function)) # NAME
+        item_name_resource.add_method(
+            "DELETE", apigateway.LambdaIntegration(llm_crud_function)
+        )  # NAME
+        item_name_resource.add_method(
+            "GET", apigateway.LambdaIntegration(llm_crud_function)
+        )  # NAME
         items_resource.add_cors_preflight(
             allow_origins=apigateway.Cors.ALL_ORIGINS,  # Allow all origins, or specify a list of allowed origins (it can be replaced with our frontend domain)
         )
         item_name_resource.add_cors_preflight(
             allow_origins=apigateway.Cors.ALL_ORIGINS,  # Allow all origins, or specify a list of allowed origins (it can be replaced with our frontend domain)
         )
-        LLMsTable = dynamodb.Table(self,id= f"{env_name}-LLMTable",table_name='LLMs'
-                                  ,partition_key= dynamodb.Attribute(name='name',type= dynamodb.AttributeType.STRING))
+        LLMsTable = dynamodb.Table(
+            self,
+            id=f"{env_name}-LLMTable",
+            table_name="LLMs",
+            partition_key=dynamodb.Attribute(
+                name="name", type=dynamodb.AttributeType.STRING
+            ),
+            sort_key=dynamodb.Attribute(
+                name='provider', type=dynamodb.AttributeType.STRING
+            )
+        )
