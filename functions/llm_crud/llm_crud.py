9e1c3060045ac73a27caa1f858bc09c1f6c519ec
Zaki Kurdya <46399191+ZakiKurdya@users.noreply.github.com>
2024-12-06 21:14:43 +0200

    Rename LLMs dynamodb table

--- functions/llm_crud/llm_crud.py
+++ functions/llm_crud/llm_crud.py
@@ -7,7 +7,7 @@ client = boto3.client("dynamodb")
 dynamodb = boto3.resource("dynamodb")
 
 env_name = os.environ.get("ENV_NAME", "Development")
-table_name = f"{env_name}-LLMs"
+table_name = f"{env_name}-LLMsMetaData"
 table = dynamodb.Table(table_name)
 
 
