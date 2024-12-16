ad2532aa1811df0daec02e8f6b563a9a13d86191
salemAmassi <salem.amassi@gmail.com>
2024-12-16 15:56:02 +0200

    Add input guardrails

---
+++ functions/llm_generation/utilites/guardrails_utils/InterestValidator.py
@@ -0,0 +1,64 @@
+from typing import Any, Dict
+from guardrails.validator_base import (
+    FailResult,
+    PassResult,
+    ValidationResult,
+    Validator,
+    register_validator,
+    ErrorSpan,
+)
+from typing import Optional, Callable
+from langchain import hub
+from litellm import completion
+import ast
+
+
+
+@register_validator(name="guardrails/interest_validator", data_type="string")
+class InterestValidator(Validator):
+    def __init__(
+        self,
+        on_fail: Optional[Callable] = None,
+        **kwargs,
+    ):
+        super().__init__(on_fail=on_fail, **kwargs)
+        self.rail_alias = "interest_validator"
+
+    def _validate(self, value: Any, metadata: Dict[str, Any]) -> ValidationResult:
+        if not isinstance(value, str):
+            return FailResult(
+                metadata=metadata,
+                error_message="Input must be a string.",
+                fix_value=None,
+            )
+
+        inference_result = self._inference_remote(interest = value, metadata=metadata)
+        validation = inference_result['decision']
+
+        return PassResult(
+                metadata= inference_result
+            )
+       
+
+    def _inference_local(self, model_input: str) -> bool:
+        """Implement a function to perform inference on a local machine."""
+        return model_input.islower()
+
+    def _inference_remote(self,metadata: dict, interest: str) -> bool:
+        """Implement a function that will build a request and perform inference on a
+        remote machine. This is not required if you will always use local mode.
+        """
+        interest_guardrail_prompt = hub.pull("interest-guardrails-prompt")
+        prompt = interest_guardrail_prompt.format(interest= interest)
+        # send request to guardrails LLM
+        evaluated_interest = completion(model=metadata['model'],
+                                        api_key= metadata['apiKey'],
+                                        messages=[{ "content": prompt,"role": "system"}])
+
+        # parse the response
+        try:    
+            evaluated_interest = ast.literal_eval(evaluated_interest['choices'][0].message['content'])
+            return evaluated_interest
+        except Exception as e:
+            return ast.literal_eval(e)
+       
