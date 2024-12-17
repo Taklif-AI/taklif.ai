3fab1149d34be4f084126a874b0ecc1551b3b100
Zaki Kurdya <46399191+ZakiKurdya@users.noreply.github.com>
2024-12-17 10:41:30 +0200

    Add llm generators

---
+++ functions/llm_generation/utilites/llm_generators/assignment.py
@@ -0,0 +1,44 @@
+from langchain_core.tracers.langchain import wait_for_all_tracers
+from langchain_community.chat_models import ChatLiteLLM
+from langchain_core.output_parsers import StrOutputParser
+from langsmith import traceable
+from langchain import hub
+import json
+
+
+class GenerateError(Exception):
+    """Custom exception for errors in the `generate` function."""
+    def __init__(self, message):
+        super().__init__(message)
+
+
+@traceable(
+# TODO: Add ls_provider, ls_model_name
+  run_type= "llm",
+  metadata={"app": "taklif.ai"},
+)
+def generate(prompt_params:dict, litellm_call:str):
+    try:
+        interest = prompt_params.get("interest")
+        general_assignment = prompt_params.get("general_assignment")
+
+        # Prepare the prompt  
+        langsmith_prompt = hub.pull("personalize-assignment-prompt")
+
+        # Invoke the LLM
+        llm = ChatLiteLLM(model=litellm_call) # example model name: "openrouter/meta-llama/llama-3.2-3b-instruct:free"
+        
+        simple_chain = langsmith_prompt | llm | StrOutputParser()
+
+        response = simple_chain.invoke(input = {
+                                                "interest": interest,
+                                                "general_assignment": general_assignment
+                                                })
+
+        return response
+    except Exception as e:
+        raise GenerateError(f"Internal Server Error: {str(e)}")
+
+    # Wait for langsmith tracer to finish    
+    finally:
+        wait_for_all_tracers()
---
+++ functions/llm_generation/utilites/llm_generators/simplify.py
@@ -0,0 +1,43 @@
+from langchain_core.tracers.langchain import wait_for_all_tracers
+from langchain_community.chat_models import ChatLiteLLM
+from langchain_core.output_parsers import StrOutputParser
+from langsmith import traceable
+from langchain import hub
+import json
+
+
+class GenerateError(Exception):
+    """Custom exception for errors in the `generate` function."""
+    def __init__(self, message):
+        super().__init__(message)
+
+        
+@traceable(
+# TODO: Add ls_provider, ls_model_name
+  run_type= "llm",
+  metadata={"app": "taklif.ai"},
+)
+def generate(prompt_params:dict, litellm_call:str): # TODO: select a predefined levels
+    try:
+        general_assignment = prompt_params.get("general_assignment")
+        level = prompt_params.get("level")
+
+        # Prepare the prompt  
+        langsmith_prompt = hub.pull(f"{level.lower}-assignment-simplify-prompt")
+
+        # Invoke the LLM
+        llm = ChatLiteLLM(model=litellm_call) # example model name: "openrouter/meta-llama/llama-3.2-3b-instruct:free"
+        
+        simple_chain = langsmith_prompt | llm | StrOutputParser()
+
+        response = simple_chain.invoke(input = {"general_assignment": general_assignment})
+
+        # Return the content from the LLM response
+        return response
+
+    except Exception as e:
+        raise GenerateError(f"Internal Server Error: {str(e)}")
+
+    # Wait for langsmith tracer to finish    
+    finally:
+        wait_for_all_tracers()
