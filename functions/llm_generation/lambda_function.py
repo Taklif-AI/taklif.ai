7a7233fbedef9a21323fe815fd4d427e21557f98
Zaki Kurdya <46399191+ZakiKurdya@users.noreply.github.com>
2024-12-20 17:54:31 +0200

    Refactor imports

--- functions/llm_generation/lambda_function.py
+++ functions/llm_generation/lambda_function.py
@@ -1,21 +1,17 @@
-import os
-import sys
-
-sys.path.append('/var/task/utilites/')
-sys.path.append(os.getenv('LAMBDA_TASK_ROOT'))
-
-from utilites.input_parser import generation_parser, simplify_parser
-from utilites.pdf_ocr import process_pdf
-from utilites.custom_exceptions import GenerateError, BadRequestError, PDFDecodingError, PDFProcessingError
-from utilites.llm_gen_utils import assignment, simplify
-from utilites.guardrails_utils.llm_guard import guard_interest, guard_assignment
-from concurrent.futures import ThreadPoolExecutor
-from langsmith import Client as LangSmith
 import json
+import os
+from concurrent.futures import ThreadPoolExecutor
 
+from langsmith import Client as LangSmith
+from utilites.custom_exceptions import (BadRequestError, GenerateError,
+                                        PDFDecodingError, PDFProcessingError)
+from utilites.guardrails_utils.llm_guard import (guard_assignment,
+                                                 guard_interest)
+from utilites.input_parser import generation_parser, simplify_parser
+from utilites.llm_gen_utils import assignment, simplify
+from utilites.pdf_ocr import process_pdf
 
-# TODO: atomic_counter_load_balancer
-
+#TODO: atomic_counter_load_balancer
 
 def handler(event, context):
     # Get the available memory in MB from the Lambda function
@@ -23,93 +19,117 @@ def handler(event, context):
     # Estimate max threads based on available memory, use 1 thread per 128 MB of memory
     max_threads = max(1, available_memory // 128)
 
-    body = json.loads(event.get('body', '{}'))
+    body = json.loads(event.get("body", "{}"))
 
     # Extract task from body
     task = body.get("task")
     if not task:
         return {
             "statusCode": 400,
-            "body": json.dumps({"error": "Bad Request: Missing 'task' in the event payload"})
+            "body": json.dumps(
+                {"error": "Bad Request: Missing 'task' in the event payload"}
+            ),
         }
-    
+
     # Parse body parameters
     params = {}
     try:
         if task == "generation":
             params = generation_parser(body)
         elif task == "simplify":
-            params = simplify_parser(body)     
+            params = simplify_parser(body)
     except BadRequestError as e:
         return {
-        "statusCode": 400,
-        "body": json.dumps({"error": e.message}),
+            "statusCode": 400,
+            "body": json.dumps({"error": e.message}),
         }
-    
-    langsmith_client = LangSmith()
-    
-    # Interest guardrails
-    interest_validation = guard_interest(interest = params['interest'], metadata = {'litellm_call': 'openrouter/meta-llama/llama-3.2-3b-instruct:free',
-                                                              'langsmith_client': langsmith_client})
-    
-    if interest_validation['decision'] == 'rejected':
-        return {
-            'statusCode': 400,
-            'body': json.dumps({'error': interest_validation['details']['decision_explain']})
 
+    # Register langsmith client
+    langsmith_client = LangSmith()
+
+    # Interest guardrails
+    interest_validation = guard_interest(
+        interest=params["interest"],
+        metadata={
+            "litellm_call": "nvidia_nim/meta/llama-3.3-70b-instruct",
+            "langsmith_client": langsmith_client,
+        },
+    )
+
+    if interest_validation["decision"] == "rejected":
+        return {
+            "statusCode": 400,
+            "body": json.dumps(
+                {"error": interest_validation["details"]["decision_explain"]}
+            ),
         }
-     
+
     # PDF assignment processing
     if task == "generation" and params.get("is_pdf") == "true":
         try:
-            params['general_assignment'] = process_pdf(params.get('general_assignment'), max_threads)
+            params["general_assignment"] = process_pdf(
+                params.get("general_assignment"), max_threads
+            )
         except PDFDecodingError as e:
             return {
                 "statusCode": 400,
-                "body": json.dumps({"error": f"PDF Decoding error occurred: {str(e)}"})
+                "body": json.dumps({"error": f"PDF Decoding error occurred: {str(e)}"}),
             }
         except PDFProcessingError as e:
             return {
                 "statusCode": 400,
-                "body": json.dumps({"error": f"PDF Processing error occurred: {str(e)}"})
+                "body": json.dumps(
+                    {"error": f"PDF Processing error occurred: {str(e)}"}
+                ),
             }
         except Exception as e:
             return {
                 "statusCode": 500,
-                "body": json.dumps({"error": f"Unexpected error: {str(e)}"})
+                "body": json.dumps({"error": f"Unexpected error: {str(e)}"}),
             }
 
     # Assignment guardrails
-    assignment_validation = guard_assignment(assignment = params['general_assignment'], metadata={'litellm_call': 'openrouter/meta-llama/llama-3.2-3b-instruct:free',
-                                                                  'langsmith_client': langsmith_client})
-    if assignment_validation['decision'] == 'rejected':
+    assignment_validation = guard_assignment(
+        assignment=params["general_assignment"],
+        metadata={
+            "litellm_call": "nvidia_nim/meta/llama-3.3-70b-instruct",
+            "langsmith_client": langsmith_client,
+        },
+    )
+    if assignment_validation["decision"] == "rejected":
         return {
-            'statusCode': 400,
-            'body': json.dumps({'error': assignment_validation['details']['decision_explain']})
+            "statusCode": 400,
+            "body": json.dumps(
+                {"error": assignment_validation["details"]["decision_explain"]}
+            ),
+        }
 
-        }  
-      
     # LLM calling
-    response = ''
+    response = ""
     try:
         if task == "generation":
-            response = assignment.generate(body.get('params'), metadata = {'litellm_call': "openrouter/meta-llama/llama-3.2-3b-instruct:free",
-                                                                           'langsmith_client': langsmith_client,
-                                                                           })
+            response = assignment.generate(
+                body.get("params"),
+                metadata={
+                    "litellm_call": "nvidia_nim/meta/llama-3.3-70b-instruct",
+                    "langsmith_client": langsmith_client,
+                },
+            )
         elif task == "simplify":
-            response = simplify.generate(body.get('params'),  metadata = {'litellm_call': "openrouter/meta-llama/llama-3.2-3b-instruct:free",
-                                                                           'langsmith_client': langsmith_client,
-                                                                           })
+            response = simplify.generate(
+                body.get("params"),
+                metadata={
+                    "litellm_call": "nvidia_nim/meta/llama-3.3-70b-instruct",
+                    "langsmith_client": langsmith_client,
+                },
+            )
     except GenerateError as e:
         return {
-        "statusCode": 500,
-        "body": json.dumps({"error": "Internal Server Error: " + str(e)}),
+            "statusCode": 500,
+            "body": json.dumps({"error": "Internal Server Error: " + str(e)}),
         }
-  
+
     # Output guardrails
 
     # Return the content from the LLM response
-    return {
-        "statusCode": 200,
-        "body": json.dumps({"response": response})
-    }
+    return {"statusCode": 200, "body": json.dumps({"response": response})}
