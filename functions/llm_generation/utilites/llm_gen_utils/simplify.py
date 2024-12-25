9895f1bc4e75813a3415b3eff9e6a273d3d4d15f
Zaki Kurdya <46399191+ZakiKurdya@users.noreply.github.com>
2024-12-25 09:51:58 +0200

    Rename rephrase task to simplify

--- functions/llm_generation/utilites/llm_gen_utils/simplify.py
+++ functions/llm_generation/utilites/llm_gen_utils/simplify.py
@@ -1,38 +1,36 @@
 from langchain_core.tracers.langchain import wait_for_all_tracers
-from utilites.custom_exceptions import GenerateError
+from utilites.custom_exceptions import GenerationError
 from langchain_community.chat_models import ChatLiteLLM
 from langchain_core.output_parsers import StrOutputParser
 from langsmith import traceable
 import json
 
 
-
 @traceable(
-# TODO: Add ls_provider, ls_model_name
-  run_type= "llm",
-  metadata={"app": "taklif.ai"},
+    name="Simplify",
+    run_type= "llm",
+    metadata={"app": "taklif.ai"},
 )
-def generate(prompt_params:dict, metadata: dict):
+def simplify(prompt_params:dict, metadata: dict):
     try:
         personalized_assignment = prompt_params.get("personalized_assignment")
         interest = prompt_params.get("interest")
-        complexity_level = prompt_params.get("complexity_level")
 
         # Prepare the prompt  
-        langsmith_prompt = metadata['langsmith_client'].pull_prompt(prompt_identifier = "rephrase-assignment-prompt")
+        langsmith_prompt = metadata['langsmith_client'].pull_prompt(prompt_identifier = "simplify-assignment-prompt")
 
         # Invoke the LLM
         llm = ChatLiteLLM(model=metadata['litellm_call']) # example model name: "openrouter/meta-llama/llama-3.2-3b-instruct:free"
         
         simple_chain = langsmith_prompt | llm | StrOutputParser()
 
-        response = simple_chain.invoke(input = {"assignment" : personalized_assignment, "interest" : interest, "complexity_level" : complexity_level })
+        response = simple_chain.invoke(input = {"assignment" : personalized_assignment, "interest" : interest})
 
         # Return the content from the LLM response
         return response
 
     except Exception as e:
-        raise GenerateError(f"Internal Server Error: {str(e)}")
+        raise GenerationError(f"Internal Server Error: {str(e)}")
 
     # Wait for langsmith tracer to finish    
     finally:
