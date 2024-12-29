6efa280a9f15e40cbc7b9e014b593c721ebde50d
Zaki Kurdya <46399191+ZakiKurdya@users.noreply.github.com>
2024-12-29 20:31:48 +0200

    Add langsmith traceable

--- functions/llm_generation/utilites/guardrails.py
+++ functions/llm_generation/utilites/guardrails.py
@@ -1,8 +1,16 @@
+from langchain_core.tracers.langchain import wait_for_all_tracers
 from utilites.llm_load_balancer import send_request
+from langsmith import traceable
+from utilites.custom_exceptions import GenerationError
 from litellm import completion
 import ast
 
 
+@traceable(
+    name="Guardrails",
+    run_type="llm",
+    metadata={"app": "taklif.ai"},
+)
 class Guardrails:
     def __init__(self):
         self.validators = [
@@ -13,25 +21,31 @@ class Guardrails:
 
 
     def validate(self, validator_type: str, content: str, metadata: dict):
-        if validator_type not in self.validators:
-            raise ValueError(f"Unknown validator type: {validator_type}")
-        
-        if validator_type == 'interest':
-            guardrail_prompt = metadata['langsmith_client'].pull_prompt(prompt_identifier = "interest-guardrails-prompt")
-            prompt = guardrail_prompt.format(interest = content)
+        try:
+            if validator_type not in self.validators:
+                raise ValueError(f"Unknown validator type: {validator_type}")
+            
+            if validator_type == 'interest':
+                guardrail_prompt = metadata['langsmith_client'].pull_prompt(prompt_identifier = "interest-guardrails-prompt")
+                prompt = guardrail_prompt.format(interest = content)
 
-            return self.litellm_request(prompt = prompt, metadata = metadata)
-        elif validator_type == 'assignment':
-            guardrail_prompt = metadata['langsmith_client'].pull_prompt(prompt_identifier = "assignment-guardrails-prompt")
-            prompt = guardrail_prompt.format(general_assignment= content)
+                return self.litellm_request(prompt = prompt, metadata = metadata)
+            elif validator_type == 'assignment':
+                guardrail_prompt = metadata['langsmith_client'].pull_prompt(prompt_identifier = "assignment-guardrails-prompt")
+                prompt = guardrail_prompt.format(general_assignment= content)
+                
+                return self.litellm_request(prompt = prompt, metadata = metadata)
+            elif validator_type == 'output':
+                guardrail_prompt = metadata['langsmith_client'].pull_prompt(prompt_identifier = "output-guardrails-prompt")
+                prompt = guardrail_prompt.format(personalized_assignment = content)
+                
+                return self.litellm_request(prompt = prompt, metadata = metadata)
+        except Exception as e:
+            raise GenerationError(f"Internal Server Error: {str(e)}")
+        # Wait for langsmith tracer to finish
+        finally:
+            wait_for_all_tracers()    
             
-            return self.litellm_request(prompt = prompt, metadata = metadata)
-        elif validator_type == 'output':
-            guardrail_prompt = metadata['langsmith_client'].pull_prompt(prompt_identifier = "output-guardrails-prompt")
-            prompt = guardrail_prompt.format(personalized_assignment = content)
-            
-            return self.litellm_request(prompt = prompt, metadata = metadata)
-        
 
     def litellm_request(self, prompt: str, metadata:dict):
         # Send request to guardrails LLM
