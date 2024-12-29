7d7b017c687ef6c5e60ca78cd9a0e1cb2d06c7fa
Zaki Kurdya <46399191+ZakiKurdya@users.noreply.github.com>
2024-12-29 22:24:16 +0200

    Refactor llm load balancer

--- functions/llm_generation/utilites/llm_load_balancer.py
+++ functions/llm_generation/utilites/llm_load_balancer.py
@@ -1,6 +1,7 @@
 from langchain_community.chat_models import ChatLiteLLMRouter
 from litellm import Router
 from langchain_core.messages import SystemMessage
+from decimal import Decimal
 import boto3
 import os
 
@@ -8,7 +9,7 @@ env_name = os.environ.get("ENV_NAME", "Development")
 
 
 def send_request(prompt: str, metadata: dict):
-    """send request to LLM using the litellm router
+    """Send request to LLM using the litellm router
 
     Args:
         prompt (str): ready prompt with parameters filled out from the client function
@@ -17,19 +18,32 @@ def send_request(prompt: str, metadata: dict):
     Returns:
         response (AIMessage): response of LLM
     """
-    model_list = get_model_list()
-    router = setup_router(model_list= model_list)
+    model_list = [
+        {
+            'model_name': item['model_name'],
+            'litellm_params': {
+                key: int(value) if isinstance(value, Decimal) else value
+                for key, value in item['litellm_params'].items()
+            }
+        }
+        for item in get_model_list()
+    ]
+    
+    router = setup_router(model_list=model_list)
+
     chat = ChatLiteLLMRouter(
         router=router,
         model_name="model_1",
     )
+
     messages = [SystemMessage(content=prompt)]
     response = chat(messages=messages)
+
     return response
 
 
 def setup_router(model_list: list):
-    """sets up the router with desired configuration
+    """Sets up the router with desired configuration
 
     Args:
         model_list (list): list of serving LLMs
@@ -44,30 +58,32 @@ def setup_router(model_list: list):
     cooldown_time = 100                                            # cooldown the deployment for 100 seconds if it num_fails > allowed_fails
     num_retries = 3                                                # number of retries for failed requests.
     retry_after = 5                                                # waits min 5s before retrying request
-    fallbacks = [{"model_1": "model_2"}, {"model_2": ["model_1"]}] # define fallbacks to each model
+    #fallbacks = [{"model_1": "model_2"}, {"model_2": ["model_1"]}] # define fallbacks to each model
 
     router = Router(
         model_list=model_list,
         routing_strategy="usage-based-routing-v2",
-        fallbacks=fallbacks,
+     #   fallbacks=fallbacks,
         enable_pre_call_checks=enable_pre_call_checks,
         allowed_fails=allowed_fails,
         cooldown_time=cooldown_time,
         num_retries=num_retries,
         retry_after=retry_after,
     )
+
     return router
 
 
-# TODO : test dynamodb that actually returns model list
 def get_model_list():
-    """returns the list of serving LLMs
+    """Returns the list of serving LLMs
 
     Returns:
         models: models list
     """
     dynamodb = boto3.resource("dynamodb")
-    table = dynamodb.Table(f"{env_name}-LLMsMetaData")
+    table = dynamodb.Table(f"{env_name}-ServingLLMs"
+    
     models_list = table.scan()
     models = models_list["Items"]
+
     return models
