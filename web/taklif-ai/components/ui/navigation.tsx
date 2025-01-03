598c73f5366c4904faba6b880f17b55e8bd18eae
shadymt54 <mu542002@gmail.com>
2025-01-03 17:40:08 +0200

    add profile page

--- web/taklif-ai/components/ui/navigation.tsx
+++ web/taklif-ai/components/ui/navigation.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import Link from "next/link";
+import { cn } from "@/lib/utils/files/truncate-file";
 import { usePathname } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { ThemeToggle } from "@/components/ui/theme-toggle";
-import { cn } from "@/lib/utils/files/truncate-file";
 import { FileText, Users, Home, HelpCircle, Settings, BookOpen, ArrowRight, Sparkles } from "lucide-react";
 import Image from "next/image";
 const navigation = [
@@ -32,7 +32,7 @@ export function Navigation() {
         </div>
 
         <div className="flex items-center gap-4">
-          {/* <Link href="/profile">
+          <Link href="../profile">
             <Button
               variant="ghost"
               size="icon"
@@ -41,13 +41,13 @@ export function Navigation() {
               <Settings className="h-5 w-5" />
               <span className="sr-only">Profile</span>
             </Button>
-          </Link> */}
+          </Link>
           <Link href="/assignment-generation">
-            <Button size="sm" className="rounded-full w-full sm:w-auto text-white-500 bg-violet-600 hover:bg-violet-700">
+            {/* <Button size="sm" className="rounded-full w-full sm:w-auto text-white-500 bg-violet-600 hover:bg-violet-700">
               Try magic now
 
               <Sparkles className="ml-2 h-4 w-4" />
-            </Button>
+            </Button> */}
           </Link>
           <ThemeToggle />
         </div>
