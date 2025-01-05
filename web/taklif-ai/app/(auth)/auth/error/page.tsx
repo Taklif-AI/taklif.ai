1a081cd3578b76baaa535b8cff6114d199a0ffce
Mohammed Zuqlam <mzuqlam@gmail.com>
2025-01-05 17:20:32 +0200

    Add login with Google implementation

---
+++ web/taklif-ai/app/(auth)/auth/error/page.tsx
@@ -0,0 +1,7 @@
+
+
+export default function Page() {
+    return (
+        <div>Oops something went wrong!</div>
+    );
+}
--- web/taklif-ai/auth.ts
+++ web/taklif-ai/auth.ts
@@ -2,8 +2,9 @@ import NextAuth, { type DefaultSession } from "next-auth"
 import { DynamoDBAdapter } from "@auth/dynamodb-adapter"
 import { client } from '@/lib/database/dynamo-client';
 import authConfig from "@/auth.config";
-import { getUserById } from "./data/user";
+import { getUserById, updateUserField } from "./data/user";
 import { JWT } from "next-auth/jwt"
+import { log } from "console";
 
 declare module "next-auth/jwt" {
     interface JWT {
@@ -25,26 +26,41 @@ export const {
     signIn,
     signOut,
 } = NextAuth({
+    pages: {
+        signIn: 'auth/sign-in',
+        error: 'auth/error',
+    },
+    events: {
+        async linkAccount({ user }) {
+            if (user.id) {
+                user.id = `USER#${user.id}`;
+                await updateUserField(user.id, 'emailVerified', new Date().toISOString());
+            }
+        }
+    },
     callbacks: {
         async session({ session, token }) {
             session.user.id = token.id as string; //extend the session with user id
             if (token.createdAt && session.user) {
                 session.user.createdAt = token.createdAt;
             }
-
             return session;
         },
         async jwt({ token, user }) {
             if (user) {
-                token.id = user.id; // extend the token with user id
+                if (!user.id?.startsWith('USER')) {
+                    user.id = `USER#${user.id}`;
+                    token.id = user.id;
+                    token.sub = user.id;
+                } else {
+                    token.id = user.id; // extend the token with user id
+                }
             }
             if (!token.sub) return token;
-
             const existingUser = await getUserById(token.sub);
 
             if (!existingUser) return token;
             token.createdAt = existingUser.createdAt;
-
             return token;
         },
     },
