b5c4d7b0a00b804ef2f05360965661d5c2b7187a
shadymt54 <mu542002@gmail.com>
2025-01-05 02:42:06 +0200

    add profile page

--- web/taklif-ai/components/ui/navigation.tsx
+++ web/taklif-ai/components/ui/navigation.tsx
@@ -1,18 +1,12 @@
-"use client";
-
+'use client';
 import Link from "next/link";
-import { cn } from "@/lib/utils/files/truncate-file";
-import { usePathname } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { ThemeToggle } from "@/components/ui/theme-toggle";
-import { FileText, Users, Home, HelpCircle, Settings, BookOpen, ArrowRight, Sparkles } from "lucide-react";
-import Image from "next/image";
-const navigation = [
-
-];
+import { FileText , Settings, Sparkles } from "lucide-react";
+import { signOut, useSession } from "next-auth/react";
 
 export function Navigation() {
-  const pathname = usePathname();
+  // const session = useSession();
 
   return (
     <header className="fixed top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
@@ -32,23 +26,20 @@ export function Navigation() {
         </div>
 
         <div className="flex items-center gap-4">
-          <Link href="../profile">
-            <Button
+        <Link href="../profile">
+             <Button
               variant="ghost"
-              size="icon"
-              className="rounded-full"
-            >
-              <Settings className="h-5 w-5" />
-              <span className="sr-only">Profile</span>
+               size="icon"
+               className="rounded-full">
+               <Settings className="h-5 w-5" />
+               <span className="sr-only">Profile</span>
+             </Button>
+           </Link>
+          {/* {session.status === 'authenticated' && (
+            <Button onClick={() => signOut()} type="submit" size="sm" className="rounded-full w-full sm:w-auto text-white-500 bg-violet-600 hover:bg-violet-700">
+              Sign out
             </Button>
-          </Link>
-          <Link href="/assignment-generation">
-            {/* <Button size="sm" className="rounded-full w-full sm:w-auto text-white-500 bg-violet-600 hover:bg-violet-700">
-              Try magic now
-
-              <Sparkles className="ml-2 h-4 w-4" />
-            </Button> */}
-          </Link>
+          )} */}
           <ThemeToggle />
         </div>
       </nav>
