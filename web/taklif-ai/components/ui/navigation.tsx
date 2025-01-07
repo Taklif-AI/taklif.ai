1c267bff9f2ef7b48ba70ba5eea7c09bcd9f93b3
Mohammed Zuqlam <mzuqlam@gmail.com>
2025-01-07 16:19:43 +0200

    Add loader to sign put button

--- web/taklif-ai/components/ui/navigation.tsx
+++ web/taklif-ai/components/ui/navigation.tsx
@@ -3,10 +3,11 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { ThemeToggle } from "@/components/ui/theme-toggle";
 import { signOut, useSession } from "next-auth/react";
-import { FileText, Settings, Sparkles } from "lucide-react";
+import { FileText, Settings } from "lucide-react";
+import { CircleLoader } from "react-spinners";
 
 export function Navigation() {
-  const session = useSession();
+  const { data: session, status } = useSession();
 
   return (
     <header className="fixed top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
@@ -26,10 +27,13 @@ export function Navigation() {
         </div>
 
         <div className="flex items-center gap-4">
-          {session.status === 'authenticated' && (
+          {status === 'authenticated' && (
             <Button onClick={() => signOut()} type="submit" size="sm" className="rounded-full w-full sm:w-auto text-white-500 bg-violet-600 hover:bg-violet-700">
               Sign out
             </Button>
+          )}
+          {status === 'loading' && (
+            <CircleLoader size={25} color="violet" />
           )}
           <Link href="/profile">
             <Button variant="ghost" size="icon" className="rounded-full">
