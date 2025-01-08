d69025f483eb700d7d00643aba03575c63e3a10d
Mohammed Zuqlam <mzuqlam@gmail.com>
2025-01-08 20:40:35 +0200

    Add 2FA functionality

---
+++ web/taklif-ai/data/two-factor-token.ts
@@ -0,0 +1,40 @@
+import { client } from '@/lib/database/dynamo-client';
+import { QueryCommand, GetCommand } from "@aws-sdk/lib-dynamodb";
+
+export const getTwoFactorTokenByToken = async (token: string) => {
+    try {
+        const params = {
+            TableName: 'next-auth',
+            Key: {
+                pk: token,
+                sk: token,
+            }
+        };
+        const result = await client.send(new GetCommand(params));
+        if (result.Item) {
+            return result.Item;
+        }
+    } catch (error) {
+        return null;
+    }
+}
+
+export const getTwoFactorTokenByEmail = async (email: string) => {
+    try {
+        const params = {
+            TableName: 'next-auth',
+            IndexName: "GSI1",
+            KeyConditionExpression: "GSI1PK = :email",
+            ExpressionAttributeValues: {
+                ":email": `TFT#${email}`,
+            },
+            Limit: 1
+        };
+        const result = await client.send(new QueryCommand(params));
+        if (result.Items && result.Items.length > 0) {
+            return result.Items[0];
+        }
+    } catch (error) {
+        return null;
+    }
+}
--- web/taklif-ai/lib/utils/tokens.ts
+++ web/taklif-ai/lib/utils/tokens.ts
@@ -1,9 +1,58 @@
 import { getVerificationTokenByEmail } from "@/data/verification-token";
 import { getPasswordResetTokenByEmail } from "@/data/password-reset-token";
+import { getTwoFactorTokenByEmail } from "@/data/two-factor-token";
 import { v4 as uuidv4 } from "uuid";
+import crypto from "crypto";
 import { client } from '@/lib/database/dynamo-client';
 import { PutCommand, DeleteCommand } from "@aws-sdk/lib-dynamodb";
 
+
+export const generateTwoFactorToken = async (
+    email: string
+) => {
+    const token = crypto.randomInt(100000, 1000000).toString(); // 6 digits number
+    const now = Math.floor(Date.now() / 1000); // Current time in seconds
+    const expires = now + 300; // expire the token in 1hour
+
+    const existingToken = await getTwoFactorTokenByEmail(email);
+
+    if (existingToken) {
+        await client.send(new DeleteCommand({
+            TableName: 'next-auth',
+            Key: {
+                pk: existingToken.pk,
+                sk: existingToken.sk,
+            },
+            ConditionExpression: "attribute_exists(pk)",
+        }))
+    }
+
+    // Create a new two factor token record in the database
+    const towFactorToken = {
+        pk: `TFT#${token}`,
+        sk: `TFT#${token}`,
+        GSI1PK: `TFT#${email}`,
+        GSI1SK: `TFT#${email}`,
+        email: email,
+        token: token,
+        expires: expires
+    };
+
+    const parmas = {
+        TableName: 'next-auth',
+        Item: towFactorToken,
+        ConditionExpression: "attribute_not_exists(pk)",
+    };
+
+    try {
+        await client.send(new PutCommand(parmas));
+        return towFactorToken;
+    } catch (error) {
+        console.error("Error inserting item:", error);
+        throw error;
+    }
+
+}
 
 export const generatePasswordResetToken = async (email: string) => {
     const token = uuidv4();
@@ -91,3 +140,4 @@ export const generateVerificationToken = async (email: string) => {
         throw error;
     }
 }
+
