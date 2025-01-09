eddd1af036be769b4be976d19761f47d82a936cd
Mohammed Zuqlam <mzuqlam@gmail.com>
2025-01-09 18:26:29 +0200

    Update the sign-out and mark file as a server component

--- web/taklif-ai/components/ui/navigation.tsx
+++ web/taklif-ai/components/ui/navigation.tsx
@@ -1,13 +1,11 @@
-'use client';
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { ThemeToggle } from "@/components/ui/theme-toggle";
-import { signOut, useSession } from "next-auth/react";
 import { FileText, Settings } from "lucide-react";
-import { CircleLoader } from "react-spinners";
+import { auth, signOut } from "@/auth";
 
-export function Navigation() {
-  const { data: session, status } = useSession();
+export async function Navigation() {
+  const session = await auth();
 
   return (
     <header className="fixed top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
@@ -27,13 +25,20 @@ export function Navigation() {
         </div>
 
         <div className="flex items-center gap-4">
-          {status === 'authenticated' && (
-            <Button onClick={() => signOut()} type="submit" size="sm" className="rounded-full w-full sm:w-auto text-white-500 bg-violet-600 hover:bg-violet-700">
+          {session && (
+            <Button onClick={async () => {
+              "use server"
+              await signOut();
+            }} type="submit" size="sm" className="rounded-full w-full sm:w-auto text-white-500 bg-violet-600 hover:bg-violet-700">
               Sign out
             </Button>
           )}
-          {status === 'loading' && (
-            <CircleLoader size={25} color="violet" />
+          {!session && (
+            <Link href="/auth/sign-in">
+              <Button size="sm" className="rounded-full w-full sm:w-auto text-white-500 bg-violet-600 hover:bg-violet-700">
+                Sign in
+              </Button>
+            </Link>
           )}
           <Link href="/profile">
             <Button variant="ghost" size="icon" className="rounded-full">
