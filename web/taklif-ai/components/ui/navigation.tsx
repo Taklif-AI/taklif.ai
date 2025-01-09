62681caf3be0441833672b4f0ba700f6cc180ed0
Mohammed Zuqlam <mzuqlam@gmail.com>
2025-01-09 20:42:36 +0200

    Update sign-in button

--- web/taklif-ai/components/ui/navigation.tsx
+++ web/taklif-ai/components/ui/navigation.tsx
@@ -1,8 +1,7 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { ThemeToggle } from "@/components/ui/theme-toggle";
-import {  Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
-import { CircleLoader } from "react-spinners";
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import Image from "next/image";
 import SVGIMG from "../../public/taklif-logo.svg";
 import { auth, signOut } from "@/auth";
@@ -19,11 +18,11 @@ export async function Navigation() {
             href="/"
             className="flex items-center space-x-2 text-lg font-bold text-foreground"
           >
-          <Image className="w-48" src={SVGIMG} alt={""}/>
+            <Image className="w-48" src={SVGIMG} alt={""} />
 
           </Link>
 
-        
+
 
           <div className="hidden md:flex md:items-center md:gap-5">
 
@@ -31,25 +30,17 @@ export async function Navigation() {
         </div>
 
         <div className="flex items-center gap-4">
-          {session && (
-            <Button onClick={async () => {
-              "use server"
-              await signOut();
-            }} type="submit" size="sm" className="rounded-full w-full sm:w-auto text-white-500 bg-violet-600 hover:bg-violet-700">
-              Sign out
-            </Button>
-          )}
+
           {!session && (
             <Link href="/auth/sign-in">
-              <Button size="sm" className="rounded-full w-full sm:w-auto text-white-500 bg-violet-600 hover:bg-violet-700">
-                Sign in
-              </Button>
-            </Link>
-          )}
               <Button variant="outline" className="rounded-full bg-transparent	"
               >
                 Sign In
               </Button>
+            </Link>
+          )}
+
+          {session && (
             <Menu as="div" className="relative ml-3">
               <div>
                 <MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
@@ -68,30 +59,23 @@ export async function Navigation() {
               >
                 <MenuItem>
                   <a
-                    href="#"
+                    href="/profile"
                     className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none"
                   >
-                    Your Profile
+                    Profile
                   </a>
                 </MenuItem>
                 <MenuItem>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none"
-                  >
-                    Settings
-                  </a>
-                </MenuItem>
-                <MenuItem>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none"
-                  >
+                  <button onClick={async () => {
+                    "use server"
+                    await signOut();
+                  }} className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none">
                     Sign out
-                  </a>
+                  </button>
                 </MenuItem>
               </MenuItems>
             </Menu>
+          )}
           <ThemeToggle />
         </div>
       </nav>
