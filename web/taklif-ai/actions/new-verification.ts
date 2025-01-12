dadbf8ddbc1bc64a442ac62d39737856d0080143
Mohammed Zuqlam <mzuqlam@gmail.com>
2025-01-12 19:53:12 +0200

    Update generateVerificationToken function

--- web/taklif-ai/actions/new-verification.ts
+++ web/taklif-ai/actions/new-verification.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { getUserByEmail, updateUserFields } from "@/data/user";
+import { getUserByEmail, updateUserDynamicData, updateUserFields } from "@/data/user";
 import { getVerificationTokenByToken } from "@/data/verification-token";
 import { client } from '@/lib/database/dynamo-client';
 import { DeleteCommand } from "@aws-sdk/lib-dynamodb";
@@ -20,13 +20,20 @@ export const newVerification = async (token: string) => {
         return { error: "Token has expired!" }
     }
 
-    const existingUser = await getUserByEmail(existingToken.email);
+    const existingUser = await getUserByEmail(existingToken.old_email);
 
     if (!existingUser) {
         return { error: "Email does not exist!" }
     }
 
-    await updateUserFields(existingUser.pk, 'emailVerified', new Date().toISOString(), 'email', existingToken.email);
+    await updateUserDynamicData(existingUser.pk,
+        {
+            emailVerified: new Date().toISOString(),
+            email: existingToken.email,
+            GSI1PK: `USER#${existingToken.email}`,
+            GSI1SK: `USER#${existingToken.email}`,
+        },
+    );
     await client.send(new DeleteCommand({
         TableName: 'next-auth',
         Key: {
@@ -36,5 +43,5 @@ export const newVerification = async (token: string) => {
         ConditionExpression: "attribute_exists(pk)",
     }))
 
-    return { success: "Email verified!" }; 
+    return { success: "Email verified!" };
 }
