8cc0173435f2728f2d7af347401b010144755ec2
Mohammed Zuqlam <mzuqlam@gmail.com>
2025-01-26 17:37:50 +0200

    Wrappe page with Suspense component

--- web/taklif-ai/app/(auth)/auth/new-verification/page.tsx
+++ web/taklif-ai/app/(auth)/auth/new-verification/page.tsx
@@ -1,10 +1,14 @@
 import { NewVerificationForm } from "@/components/auth/new-verification-form";
+import { Suspense } from 'react'
 
 
 const NewVerificationPage = () => {
 
     return (
-        <NewVerificationForm />
+        <Suspense>
+            <NewVerificationForm />
+        </Suspense>
+        
     )
 }
 
