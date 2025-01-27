35e463a9907aac46e2e188f3f257462a528efcc8
Mohammed Zuqlam <mzuqlam@gmail.com>
2025-01-27 20:33:45 +0200

    Update S3 bucket name

--- web/taklif-ai/actions/upload-image.ts
+++ web/taklif-ai/actions/upload-image.ts
@@ -75,7 +75,7 @@ export const uploadImage = async (filename: string, contentType: string) => {
 async function deleteOldProfileImage(s3Key: string) {
     try {
         const deleteParams = {
-            Bucket: process.env.S3_BUCKET_NAME,
+            Bucket: "development-user-images-bucket",
             Key: s3Key,
         };
         await s3.send(new DeleteObjectCommand(deleteParams));
