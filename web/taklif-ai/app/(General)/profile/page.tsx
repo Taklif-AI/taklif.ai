6f593ca23e3fcf5893f8b08e8791f6484d1d062d
Mohammed Zuqlam <mzuqlam@gmail.com>
2025-01-28 18:00:24 +0200

    Update the session with user image

--- web/taklif-ai/app/(General)/profile/page.tsx
+++ web/taklif-ai/app/(General)/profile/page.tsx
@@ -111,7 +111,7 @@ export default function ProfilePage() {
           }
 
           if (data.fields && data.url && data.s3Key && data.imageUrl) {
-            update();
+            update({ image: data.imageUrl });
 
             // Upload the image to S3
             const formData = new FormData();
