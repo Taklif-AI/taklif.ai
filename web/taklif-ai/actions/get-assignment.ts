851ca42293c5a9e397c74a7eca752736df05ff4d
Mohammed Zuqlam <mzuqlam@gmail.com>
2025-02-15 19:45:50 +0200

    Update fetch assignment

--- web/taklif-ai/actions/get-assignment.ts
+++ web/taklif-ai/actions/get-assignment.ts
@@ -4,7 +4,11 @@ import { currentUser } from "@/lib/auth/auth";
 import { client } from "@/lib/database/dynamo-assignment-client";
 import { GetCommand } from "@aws-sdk/lib-dynamodb";
 
-export const fetchAssignment = async (run_id: string, personalization_id: string) => {
+export const fetchAssignment = async (
+    run_id: string,
+    personalization_id: string,
+    simplification_id?: string
+) => {
 
     const user = await currentUser();
     if (!user) {
@@ -14,23 +18,28 @@ export const fetchAssignment = async (run_id: string, personalization_id: string
     if (!run_id || !personalization_id) {
         return { error: 'Missing parameters' };
     }
+    console.log(run_id);
+    console.log(personalization_id);
 
     const PK = user.id;
-    const SK = `RUN#${run_id}#PERSONALIZATION#${personalization_id}`;
-
+    let SK;
+    if (simplification_id) {
+        SK = `RUN#${run_id}#PERSONALIZATION#${personalization_id}#SIMPLIFICATION#${simplification_id}`;
+    } else {
+        SK = `RUN#${run_id}#PERSONALIZATION#${personalization_id}`;
+    }
     try {
         const params = {
             TableName: 'Development-AssignmentsTable',
             Key: { PK, SK }
         }
-        
+
         const result = await client.send(new GetCommand(params));
-        
+
         if (!result.Item) {
             return { error: "Assignment not found" };
         }
-        console.log(result.Item);
-        
+
         return { data: result.Item };
     } catch (error) {
         return { error: "Server error!" };
