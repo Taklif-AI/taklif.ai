192779b035ee3e92897c9fd2f3736ad2a8094eb9
Mohammed Zuqlam <mzuqlam@gmail.com>
2025-02-17 14:02:32 +0200

    Change env variable name

--- web/taklif-ai/lib/database/dynamo-assignment-client.ts
+++ web/taklif-ai/lib/database/dynamo-assignment-client.ts
@@ -4,10 +4,10 @@ import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
 
 const config: DynamoDBClientConfig = {
     credentials: {
-        accessKeyId: process.env.ASSIGNMENT_DYNAMOD_ID as string,
-        secretAccessKey: process.env.ASSIGNMENT_DYNAMOD_SECRET as string,
+        accessKeyId: process.env.ASSIGNMENT_DYNAMODB_ID as string,
+        secretAccessKey: process.env.ASSIGNMENT_DYNAMODB_SECRET as string,
     },
-    region: process.env.ASSIGNMENT_DYNAMOD_REGION,
+    region: process.env.ASSIGNMENT_DYNAMODB_REGION,
 }
 
 export const client = DynamoDBDocument.from(new DynamoDB(config), {
