872574abbe625b933c080ba3c7a5b32605591368
Mohammed Zuqlam <mzuqlam@gmail.com>
2025-03-05 21:29:42 +0200

    Fix theme bugs

--- web/taklif-ai/app/layout.tsx
+++ web/taklif-ai/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
+import { currentUser } from "@/lib/auth/auth";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -21,12 +22,13 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const user = await currentUser();
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={poppins.className}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
+          defaultTheme={user?.theme || 'dark'}
           enableSystem
           disableTransitionOnChange
         >
