db906d0a530e25fb60f602ceae37f8c1a74d132b
Zaki Kurdya <46399191+ZakiKurdya@users.noreply.github.com>
2025-03-07 16:31:56 +0200

    Enhance profile page with subscription details and credits display

--- web/taklif-ai/auth.ts
+++ web/taklif-ai/auth.ts
@@ -2,7 +2,7 @@ import NextAuth, { type DefaultSession } from "next-auth";
 import { DynamoDBAdapter } from "@auth/dynamodb-adapter";
 import { client } from "@/lib/database/dynamo-client";
 import authConfig from "@/auth.config";
-import { getUserById, updateUserDynamicData, updateUserFields } from "./data/user";
+import { getUserById, updateUserDynamicData } from "./data/user";
 import {
   deleteTwoFactorConfirmation,
   getTwoFactorConfirmationByUserId,
@@ -15,23 +15,32 @@ declare module "next-auth/jwt" {
     createdAt?: string;
     isTwoFactorEnabled: boolean;
     isOAuth: boolean;
-    institution: string | undefined;
+    institution?: string;
     image?: string;
     s3Key?: string;
     theme: string;
+    plan: string;
+    plan_credits: number;
+    remaining_credits: number;
+    subscription_date: Date;
   }
 }
 
 declare module "next-auth" {
   interface Session {
     user: {
+      id: string;
       createdAt: string;
       isTwoFactorEnabled: boolean;
       isOAuth: boolean;
-      institution: string | undefined;
-      image: string;
-      s3Key: string;
+      institution?: string;
+      image?: string;
+      s3Key?: string;
       theme: string;
+      plan: string;
+      plan_credits: number;
+      remaining_credits: number;
+      subscription_date: Date;
     } & DefaultSession["user"];
   }
 }
@@ -45,7 +54,7 @@ export const { handlers, auth, signIn, signOut, unstable_update } = NextAuth({
     async linkAccount({ user }) {
       if (user.id) {
         user.id = `USER#${user.id}`;
-        await updateUserDynamicData(user.id as string, {
+        await updateUserDynamicData(user.id, {
           emailVerified: new Date().toISOString(),
           createdAt: new Date().toISOString(),
           subscription: {
@@ -54,28 +63,23 @@ export const { handlers, auth, signIn, signOut, unstable_update } = NextAuth({
             remaining_credits: 60,
             subscription_date: new Date().toISOString(),
           },
-        })
+        });
       }
     },
   },
   callbacks: {
     async signIn({ user, account }) {
-      // Allow OAuth without email verification
       if (account?.provider !== "credentials") return true;
 
       const existingUser = await getUserById(user.id as string);
-
-      // prevent login if email is not verified yet
       if (!existingUser?.emailVerified) return false;
 
       if (existingUser.isTwoFactorEnabled) {
         const twoFactorConfirmation = await getTwoFactorConfirmationByUserId(
           existingUser.pk,
         );
-
         if (!twoFactorConfirmation) return false;
 
-        // Delete two factor confirmationfor next sign in process
         await deleteTwoFactorConfirmation(
           twoFactorConfirmation.pk,
           twoFactorConfirmation.sk,
@@ -84,52 +88,58 @@ export const { handlers, auth, signIn, signOut, unstable_update } = NextAuth({
       return true;
     },
     async session({ session, token }) {
-      session.user.id = token.id as string; //extend the session with user id
-      if (token.createdAt && session.user) {
-        session.user.createdAt = token.createdAt;
-      }
       if (session.user) {
+        session.user.id = token.id as string;
+        session.user.createdAt = token.createdAt || "";
         session.user.isTwoFactorEnabled = token.isTwoFactorEnabled;
         session.user.name = token.name;
-        session.user.email = token.email as string;
-        session.user.image = token.image as string;
-        session.user.s3Key = token.s3Key as string;
+        session.user.email = token.email || "";
+        session.user.image = token.image || "";
+        session.user.s3Key = token.s3Key || "";
         session.user.isOAuth = token.isOAuth;
-        session.user.institution = token.institution || undefined;
+        session.user.institution = token.institution;
         session.user.theme = token.theme;
+        session.user.plan = token.plan;
+        session.user.plan_credits = token.plan_credits;
+        session.user.remaining_credits = token.remaining_credits;
+        session.user.subscription_date = token.subscription_date;
       }
-
       return session;
     },
     async jwt({ token, user }) {
       if (user) {
         if (!user.id?.startsWith("USER")) {
           user.id = `USER#${user.id}`;
-          token.id = user.id;
-          token.sub = user.id;
-        } else {
-          token.id = user.id; // extend the token with user id
         }
+        token.id = user.id;
+        token.sub = user.id;
       }
-      if (!token.sub) return token;
-      const existingUser = await getUserById(token.sub);
 
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
       if (!existingUser) return token;
 
       const existingAccount = await getAccountByUserId(existingUser.pk);
-
       token.isOAuth = !!existingAccount;
 
       token.name = existingUser.name;
       token.email = existingUser.email;
       token.image = existingUser.image;
       token.s3Key = existingUser.s3Key;
-      token.institution = existingUser.institution || undefined;
+      token.institution = existingUser.institution;
       token.createdAt = existingUser.createdAt;
       token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
       token.theme = existingUser.theme;
 
-      // Set the JWT token expiration to match the session maxAge
+      // Extracting `subscription` data correctly from the map
+      token.plan = existingUser.subscription?.plan || "free";
+      token.plan_credits = existingUser.subscription?.plan_credits;
+      token.remaining_credits = existingUser.subscription?.remaining_credits;
+      token.subscription_date = existingUser.subscription?.subscription_date
+        ? new Date(existingUser.subscription.subscription_date)
+        : new Date();
+
       token.exp = Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 10; // 10 days
       return token;
     },
@@ -137,7 +147,7 @@ export const { handlers, auth, signIn, signOut, unstable_update } = NextAuth({
   adapter: DynamoDBAdapter(client),
   session: {
     strategy: "jwt",
-    maxAge: 60 * 60 * 24 * 10, // 30 days
+    maxAge: 60 * 60 * 24 * 10, // 10 days
   },
   ...authConfig,
 });
