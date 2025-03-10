efad10ea88c1cc9e413189e9dc98d1451a1393e7
Mohammed Zuqlam <mzuqlam@gmail.com>
2025-03-11 00:22:17 +0200

    Update sign-up schema

--- web/taklif-ai/lib/schemas/register-schema.ts
+++ web/taklif-ai/lib/schemas/register-schema.ts
@@ -1,9 +1,13 @@
 import * as z from "zod";
 
 export const RegisterSchema = z.object({
-  name: z.string().min(1, {
-    message: "Name is required",
-  }),
+  name: z.string()
+    .min(2, { message: "Name must be at least 2 characters." })
+    .max(20, { message: 'Name must be at most 20 characters.' })
+    .regex(/^[\p{L}\p{M}\s.'-]+$/u, {
+      message:
+        "Name can only contain letters, spaces, apostrophes, hyphens, and periods.",
+    }),
   email: z.string().email({
     message: "Email is required",
   }),
