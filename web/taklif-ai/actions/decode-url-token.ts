2cdece7262d2d4c438492ab2b67ed3f4522d4f57
Mohammed Zuqlam <mzuqlam@gmail.com>
2025-03-11 21:20:07 +0200

    Add decode token action

---
+++ web/taklif-ai/actions/decode-url-token.ts
@@ -0,0 +1,26 @@
+"use server";
+
+import { currentUser } from '@/lib/auth/auth';
+import jwt from 'jsonwebtoken';
+
+const secret = process.env.JWT_SECRET;
+export async function decodeUrlToken(token: string) {
+
+    const user = await currentUser();
+    if (!user) {
+        return { error: "Unauthorized" };
+    }
+
+    if (!token) {
+        return { error: 'Missing required token' }
+    }
+
+    try {
+        const decoded = jwt.verify(token, secret as string);
+        const { run_id, personalization_id } = decoded as { run_id: string, personalization_id: string };
+        return { run_id, personalization_id };
+    } catch (error) {
+        console.log(error);
+        return { error: 'Failed to decode token' };
+    }
+}
