2d03e415f4614b05cfeaa7cbfdfd367e6a0dd063
Mohammed Zuqlam <mzuqlam@gmail.com>
2025-03-12 22:19:55 +0200

    Update login schema

--- web/taklif-ai/lib/schemas/login-schema.ts
+++ web/taklif-ai/lib/schemas/login-schema.ts
@@ -1,9 +1,22 @@
 import * as z from "zod";
 
 export const LoginSchema = z.object({
-  email: z.string().email({
-    message: "Email is required",
-  }),
-  password: z.string().min(1, { message: "Password is required" }),
-  code: z.optional(z.string()),
+  email:
+    z.preprocess(
+      (val) => (typeof val === "string" ? val.trim() : val),
+      z.string().email({
+        message: "Email is required",
+      }),
+    ),
+
+  password:
+    z.preprocess(
+      (val) => (typeof val === "string" ? val.trim() : val),
+      z.string().min(1, { message: "Password is required" }),
+    ),
+  code:
+    z.preprocess(
+      (val) => (typeof val === "string" ? val.trim() : val),
+      z.optional(z.string()),
+    )
 });
