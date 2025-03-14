82a04ab401ef499c675472b0c221c6999f770ef1
Mohammed Zuqlam <mzuqlam@gmail.com>
2025-03-14 22:51:51 +0200

    Update settings action

--- web/taklif-ai/actions/settings.ts
+++ web/taklif-ai/actions/settings.ts
@@ -5,6 +5,7 @@ import { currentUser } from "@/lib/auth/auth";
 import { SettingsSchema } from "@/lib/schemas/settings-schema";
 import { unstable_update } from "@/auth";
 import bcrypt from "bcryptjs";
+import { PasswordsSchema } from "@/lib/schemas/change-password-schema";
 
 export const settings = async (formData) => {
   const data = {};
@@ -14,61 +15,26 @@ export const settings = async (formData) => {
     return { error: "Unauthorized" };
   }
 
-  // if (user.isOAuth) {
-  //   formData.password = undefined;
-  //   formData.newPassword = undefined;
-  //   formData.isTwoFactorEnabled = undefined;
-  // }
-
   const dbUser = await getUserById(user.id as string);
   if (!dbUser) {
     return { error: "Unauthorized" };
   }
 
   if (!user.isOAuth) {
-    if (formData.password && !formData.newPassword) {
-      return { error: " New password is required!" };
-    }
-
-    if (formData.newPassword && !formData.password) {
-      return { error: "Password is required!" };
-    }
-
-    if (formData.password && formData.newPassword && dbUser.password) {
-      const validateData = SettingsSchema.safeParse({
-        password: formData.password,
-        newPassword: formData.newPassword,
-      });
-
-      if (!validateData.success) {
-        const errors = validateData.error.errors.map((err) => err.message);
-
-        return { error: errors[0] };
-      }
-      const { password, newPassword } = validateData.data;
-      const passwordsMatch = await bcrypt.compare(
-        password as string,
-        dbUser.password,
-      );
-      if (!passwordsMatch) {
-        return { error: "Incorrect password" };
-      }
-
-      const hashedPassword = await bcrypt.hash(newPassword as string, 10);
-      data.password = hashedPassword;
-    }
-
     const validatedIsTwoFactorEnabled = SettingsSchema.safeParse({
       isTwoFactorEnabled: formData.isTwoFactorEnabled,
     });
+
     if (!validatedIsTwoFactorEnabled.success) {
       const errors = validatedIsTwoFactorEnabled.error.errors.map(
         (err) => err.message,
       );
       return { error: errors[0] };
     }
+
     const { isTwoFactorEnabled } = validatedIsTwoFactorEnabled.data;
     data.isTwoFactorEnabled = isTwoFactorEnabled;
+    
     if (isTwoFactorEnabled === true || isTwoFactorEnabled === false) {
       await unstable_update({
         user: {
