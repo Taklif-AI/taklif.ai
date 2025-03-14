3b1f362d1aeaad26a7469b92c8007c16986d9d60
Mohammed Zuqlam <mzuqlam@gmail.com>
2025-03-14 22:51:38 +0200

    Create change password action

---
+++ web/taklif-ai/lib/schemas/change-password-schema.ts
@@ -0,0 +1,26 @@
+import * as z from "zod";
+
+export const PasswordsSchema = z.object({
+  password:
+    z.preprocess(
+      (val) => (typeof val === "string" ? val.trim() : val),
+      z.string()
+    ),
+  newPassword:
+    z.preprocess(
+      (val) => (typeof val === "string" ? val.trim() : val),
+      z.string()
+        .min(8, { message: "New password must be at least 8 characters long" })
+        .regex(/[A-Z]/, {
+          message: "New password must include at least one uppercase letter",
+        })
+        .regex(/[a-z]/, {
+          message: "New password must include at least one lowercase letter",
+        })
+        .regex(/\d/, { message: "New password must include at least one number" })
+        .regex(/[@$!%*?&]/, {
+          message:
+            "New password must include at least one special character (@, $, !, %, *, ?, &)",
+        }),
+    ),
+});
